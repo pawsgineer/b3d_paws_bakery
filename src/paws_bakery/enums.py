68088064f3192c5c83b0219a63995fac1a583bbd
Steve (Pawsgineer) Paws <188218628+pawsgineer@users.noreply.github.com>
2025-08-23 00:27:24 +0400

    fix: better handling of image editor discovery
    * Look in all windows
    * Ignore pinned and 'Render Result'

--- src/paws_bakery/enums.py
+++ src/paws_bakery/enums.py
@@ -49,6 +49,48 @@ class BlenderJobType(CaseSensitiveStrEnum):
     OBJECT_BAKE = auto()
 
 
+class BlenderImageType(CaseSensitiveStrEnum):
+    """Blender Image types.
+
+    List may not be complete and contain only used types.
+    """
+
+    IMAGE = auto()
+    MULTILAYER = auto()
+    UV_TEST = auto()
+    RENDER_RESULT = auto()
+    COMPOSITING = auto()
+
+
+class BlenderSpaceType(CaseSensitiveStrEnum):
+    """Blender Space types.
+
+    List may not be complete and contain only used types.
+    """
+
+    VIEW_3D = auto()
+    IMAGE_EDITOR = auto()
+    NODE_EDITOR = auto()
+    SEQUENCE_EDITOR = auto()
+    CLIP_EDITOR = auto()
+
+    DOPESHEET_EDITOR = auto()
+    GRAPH_EDITOR = auto()
+    NLA_EDITOR = auto()
+
+    TEXT_EDITOR = auto()
+    CONSOLE = auto()
+    INFO = auto()
+    TOPBAR = auto()
+    STATUSBAR = auto()
+
+    OUTLINER = auto()
+    PROPERTIES = auto()
+    FILE_BROWSER = auto()
+    SPREADSHEET = auto()
+    PREFERENCES = auto()
+
+
 class CyclesBakeType(CaseSensitiveStrEnum):
     """Blender cycles bake type names used in addon.
 
