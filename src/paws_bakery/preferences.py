429fed532742844fc02a68bd5a70ab28dd18e998
Steve (Pawsgineer) Paws <188218628+pawsgineer@users.noreply.github.com>
2025-08-22 22:06:06 +0400

    fix: filepath does not support relative error

--- src/paws_bakery/preferences.py
+++ src/paws_bakery/preferences.py
@@ -1,5 +1,7 @@
 """UI Panel - Preferences."""
 
+from typing import cast
+
 import bpy
 from bpy import props as b_p
 from bpy import types as b_t
@@ -13,14 +15,15 @@ class AddonPreferences(b_t.AddonPreferences):
 
     bl_idname = __package__
 
-    output_directory: b_p.StringProperty(
+    output_directory: b_p.StringProperty(  # type: ignore[valid-type]
         name="Output Directory",
         description="Directory to save baked images",
         default="//pawsbkr_textures",
         subtype="DIR_PATH",  # noqa: F821
+        options={"PATH_SUPPORTS_BLEND_RELATIVE"},  # noqa: F821
     )
 
-    enable_debug_tools: b_p.BoolProperty(
+    enable_debug_tools: b_p.BoolProperty(  # type: ignore[valid-type]
         name="Enable Debug Tools",
         description=(
             "Enable development utils. Use only if you know what you're doing."
@@ -28,7 +31,7 @@ class AddonPreferences(b_t.AddonPreferences):
         default=False,
     )
 
-    def draw(self, _context: b_t.Context):
+    def draw(self, _context: b_t.Context) -> None:
         """draw() override."""
         layout = self.layout
         layout.prop(self, "output_directory")
@@ -36,5 +39,7 @@ class AddonPreferences(b_t.AddonPreferences):
 
 
 def get_preferences() -> AddonPreferences:
-    """Returns registered addon preferences."""
-    return bpy.context.preferences.addons[__package__].preferences
+    """Return registered addon preferences."""
+    return cast(
+        AddonPreferences, bpy.context.preferences.addons[__package__].preferences
+    )
