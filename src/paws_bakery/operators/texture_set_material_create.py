ac0d0347b8f405288746701bced01b970bdcf04c
Steve (Pawsgineer) Paws <188218628+pawsgineer@users.noreply.github.com>
2025-08-26 10:21:28 +0400

    feat: per-object bake mode (experimental)

--- src/paws_bakery/operators/texture_set_material_create.py
+++ src/paws_bakery/operators/texture_set_material_create.py
@@ -1,6 +1,8 @@
 """Create Materials for Texture Set."""
 
 from collections.abc import Sequence
+from dataclasses import dataclass
+from itertools import chain
 from typing import cast
 
 import bpy
@@ -10,6 +12,7 @@ from bpy import types as b_t
 from .._helpers import log, log_err
 from ..enums import BlenderOperatorReturnType, BlenderWMReportType
 from ..props import MeshProps, TextureSetProps, get_bake_settings, get_props
+from ..props_enums import BakeMode
 from ..utils import AddonException, AssetLibraryManager, Registry
 from .bake_common import (
     ensure_mesh_ref,
@@ -17,15 +20,6 @@ from .bake_common import (
     match_low_to_high,
 )
 from .texture_import import UTIL_MATS_IMPORT_SAMPLE_NAME, assign_images_to_material
-
-
-def generate_material_name(*, context: b_t.Context, texture_set_name: str) -> str:
-    """Return generated material name."""
-    utils_settings = get_props(context).utils_settings
-    prefix = utils_settings.material_creation.name_prefix
-    suffix = utils_settings.material_creation.name_suffix
-
-    return f"{prefix}{texture_set_name}{suffix}"
 
 
 @Registry.add
@@ -61,44 +55,11 @@ class TextureSetMaterialCreate(b_t.Operator):
         return {BlenderOperatorReturnType.FINISHED}
 
 
-def _setup_material(
-    *,
-    context: b_t.Context,
-    images: Sequence[b_t.Image],
-    template_material: b_t.Material,
-    texture_set: TextureSetProps,
-    recreate: bool,
-) -> b_t.Material:
-    """Assign images to material.
-
-    Creates new material if not exists.
-    """
-    mat_name = generate_material_name(
-        context=context, texture_set_name=texture_set.display_name
-    )
-    mat = bpy.data.materials.get(mat_name)
-
-    if recreate and mat:
-        bpy.data.materials.remove(mat)
-        mat = None
-
-    if not mat:
-        log(
-            f"Material {mat_name!r} doesn't exist, creating from "
-            f"{template_material!r}"
-        )
-        mat = cast(b_t.Material, template_material.copy())
-        mat.name = mat_name
-
-    opts = get_props(context).utils_settings.material_creation
-    if opts.mark_as_asset:
-        mat.asset_mark()  # type: ignore[no-untyped-call]
-    if opts.use_fake_user:
-        mat.use_fake_user = True
-
-    assign_images_to_material(mat, images, True)
-
-    return mat
+@dataclass(kw_only=True)
+class _MaterialUpdateInfo:
+    name: str
+    meshes: list[b_t.Object]
+    images: list[b_t.Image]
 
 
 def create_materials(*, context: b_t.Context, texture_set: TextureSetProps) -> None:
@@ -109,31 +70,67 @@ def create_materials(*, context: b_t.Context, texture_set: TextureSetProps) -> N
             UTIL_MATS_IMPORT_SAMPLE_NAME
         )
 
-    images: dict[str, b_t.Image] = {}
-    images_missing = []
+    meshes_to_update = _get_meshes_to_update(context=context, texture_set=texture_set)
 
-    for texture_props in texture_set.get_enabled_textures():
-        bake_settings = get_bake_settings(context, texture_props.prop_id)
+    mat_info_list: list[_MaterialUpdateInfo] = []
+    if BakeMode[texture_set.mode] is BakeMode.PER_OBJECT:
+        for mesh in meshes_to_update:
+            mat_info_list.append(
+                _MaterialUpdateInfo(
+                    name=_generate_material_name(
+                        context=context,
+                        texture_set_name=texture_set.display_name,
+                        object_prefix=mesh.name,
+                    ),
+                    meshes=[mesh],
+                    images=_load_images(
+                        context=context,
+                        texture_set=texture_set,
+                        object_prefix=mesh.name,
+                    ),
+                )
+            )
+    else:
+        mat_info_list.append(
+            _MaterialUpdateInfo(
+                name=_generate_material_name(
+                    context=context, texture_set_name=texture_set.display_name
+                ),
+                meshes=meshes_to_update,
+                images=_load_images(context=context, texture_set=texture_set),
+            )
+        )
 
-        img_name, _ = generate_image_name_and_path(
+    for mat_info in mat_info_list:
+        if texture_set.create_materials_reuse_existing:
+            for mat in set(
+                chain.from_iterable(
+                    _get_object_materials(mesh) for mesh in mat_info.meshes
+                )
+            ):
+                assign_images_to_material(mat, mat_info.images, True)
+            continue
+
+        mat = _setup_material(
             context=context,
-            settings_id=texture_props.prop_id,
-            texture_set_name=texture_set.display_name,
+            name=mat_info.name,
+            images=mat_info.images,
+            template_material=template_material,
+            recreate=True,
         )
 
-        image = bpy.data.images.get(img_name)
-        if image:
-            images[bake_settings.type] = image
-        else:
-            images_missing.append(img_name)
+        if not texture_set.create_materials_assign_to_objects:
+            continue
 
-    if not images or images_missing:
-        raise AddonException(
-            "No baked images found. Bake textures first. Images missing: "
-            f"{images_missing}"
-        )
+        for mesh in mat_info.meshes:
+            for slot in mesh.material_slots:
+                slot.material = mat
 
-    meshes_enabled: Sequence[MeshProps] = texture_set.get_enabled_meshes()
+
+def _get_meshes_to_update(
+    *, context: b_t.Context, texture_set: TextureSetProps
+) -> list[b_t.Object]:
+    meshes_enabled: list[MeshProps] = texture_set.get_enabled_meshes()
     bake_settings = get_bake_settings(context, texture_set.textures[0].prop_id)
     if bake_settings.use_selected_to_active and bake_settings.match_active_by_suffix:
         matching_names = match_low_to_high([m.name for m in meshes_enabled])
@@ -144,32 +141,93 @@ def create_materials(*, context: b_t.Context, texture_set: TextureSetProps) -> N
     else:
         meshes_to_update = [ensure_mesh_ref(m) for m in meshes_enabled]
 
-    if texture_set.create_materials_reuse_existing:
-        materials_to_update: set[b_t.Material] = set()
-        for mesh_ref in meshes_to_update:
-            for slot in mesh_ref.material_slots:
-                if not slot.material:
-                    continue
-                materials_to_update.add(slot.material)
+    return meshes_to_update
 
-        for mat in materials_to_update:
-            assign_images_to_material(mat, list(images.values()), True)
 
-        return
+def _generate_material_name(
+    *, context: b_t.Context, texture_set_name: str, object_prefix: str = ""
+) -> str:
+    """Return generated material name."""
+    utils_settings = get_props(context).utils_settings
+    prefix = utils_settings.material_creation.name_prefix
+    suffix = utils_settings.material_creation.name_suffix
+    if object_prefix:
+        texture_set_name = "_".join([object_prefix, texture_set_name])
 
-    mat = _setup_material(
-        context=context,
-        images=list(images.values()),
-        texture_set=texture_set,
-        template_material=template_material,
-        recreate=True,
-    )
+    return f"{prefix}{texture_set_name}{suffix}"
 
-    if not texture_set.create_materials_assign_to_objects:
-        return
 
-    for mesh_ref in meshes_to_update:
-        for slot in mesh_ref.material_slots:
-            # if not slot.material:
-            #     continue
-            slot.material = mat
+def _load_images(
+    *,
+    context: b_t.Context,
+    texture_set: TextureSetProps,
+    object_prefix: str = "",
+) -> list[b_t.Image]:
+    images: list[b_t.Image] = []
+    images_missing: list[str] = []
+
+    for texture_props in texture_set.get_enabled_textures():
+        img_name, _ = generate_image_name_and_path(
+            context=context,
+            settings_id=texture_props.prop_id,
+            texture_set_name=texture_set.display_name,
+            object_prefix=object_prefix,
+        )
+
+        image = bpy.data.images.get(img_name)
+        if image:
+            images.append(image)
+        else:
+            images_missing.append(img_name)
+
+    if not images or images_missing:
+        raise AddonException(
+            "No baked images found. Bake textures first. Images missing: "
+            f"{images_missing}"
+        )
+
+    return images
+
+
+def _get_object_materials(bobj: b_t.Object) -> set[b_t.Material]:
+    mats: set[b_t.Material] = set()
+    for slot in bobj.material_slots:
+        if not slot.material:
+            continue
+        mats.add(slot.material)
+
+    return mats
+
+
+def _setup_material(
+    *,
+    context: b_t.Context,
+    name: str,
+    images: Sequence[b_t.Image],
+    template_material: b_t.Material,
+    recreate: bool,
+) -> b_t.Material:
+    """Assign images to material.
+
+    Creates new material if not exists.
+    """
+    mat: b_t.Material | None = bpy.data.materials.get(name)
+
+    if recreate and mat:
+        bpy.data.materials.remove(mat)
+        mat = None
+
+    if not mat:
+        log(f"Material {name!r} doesn't exist, creating from {template_material!r}")
+        mat = cast(b_t.Material, template_material.copy())
+        mat.name = name
+
+    opts = get_props(context).utils_settings.material_creation
+    if opts.mark_as_asset:
+        mat.asset_mark()  # type: ignore[no-untyped-call]
+    if opts.use_fake_user:
+        mat.use_fake_user = True
+
+    assign_images_to_material(mat, images, True)
+
+    return mat
